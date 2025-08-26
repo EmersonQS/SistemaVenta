b51977f20ce78302550138090a1850631e457532
EmersonQS <emer200006@gmail.com>
2025-08-26 16:37:42 -0500

    Primer commit del proyecto SistemaVenta

---
+++ resources/views/plantilla/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="app-footer">
+        <!--begin::To the end-->
+        <div class="float-end d-none d-sm-inline">Sistema Venta</div>
+        <!--end::To the end-->
+        <!--begin::Copyright-->
+        <strong>
+          Copyright &copy; 2025&nbsp;
+          <a href="https://www.fullstack.pe/" class="text-decoration-none">Fullstack.pe</a>.
+        </strong>
+        All rights reserved.
+        <!--end::Copyright-->
+      </footer>
